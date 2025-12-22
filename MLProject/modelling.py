b86cdc09fb25441a5f2faddaa40e9b5545c27476
Muhammad Dzaky Irawan <mdzakyirawan@gmail.com>
2025-12-22 10:43:41 +0800

    Add file

---
+++ MLProject/modelling.py
@@ -0,0 +1,27 @@
+import mlflow
+import mlflow.sklearn
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+mlflow.set_experiment("workflow-ci-experiment")
+
+with mlflow.start_run():
+    df = pd.read_csv("dataset_preprocessed.csv")
+
+    X = df.drop("target", axis=1)
+    y = df["target"]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42
+    )
+
+    model = RandomForestClassifier(random_state=42)
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
+
+    mlflow.log_metric("accuracy", acc)
+    mlflow.sklearn.log_model(model, "model")
