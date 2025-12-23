f6ea2196bc4c30c155aa57fce7a461ab9e2674a4
Muhammad Dzaky Irawan <144348757+mdzakyirawan@users.noreply.github.com>
2025-12-23 15:49:17 +0800

    Refactor model training to use sklearn pipeline
    Refactor the main function to use a pipeline for model training and logging with MLflow.

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,48 +1,46 @@
-import os
+import pandas as pd
 import mlflow
 import mlflow.sklearn
-import pandas as pd
 
 from sklearn.model_selection import train_test_split
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-def main():
-    # Reset MLflow state (CI safe)
-    if "MLFLOW_RUN_ID" in os.environ:
-        del os.environ["MLFLOW_RUN_ID"]
+DATA_PATH = "dataset_preprocessed.csv"
+TARGET_COLUMN = "RiskLevel_encoded"
 
-    mlflow.end_run()
+df = pd.read_csv(DATA_PATH)
 
-    mlflow.set_tracking_uri("file:./mlruns")
-    mlflow.set_experiment("workflow-ci-experiment")
+X = df.drop(columns=[TARGET_COLUMN])
+y = df[TARGET_COLUMN]
 
-    df = pd.read_csv("dataset_preprocessed.csv")
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42,
+    stratify=y
+)
 
-    target_col = df.columns[-1]
-    X = df.drop(columns=[target_col])
-    y = df[target_col]
+mlflow.set_experiment("Eksperimen_Model_Pipeline")
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=42
-    )
+with mlflow.start_run():
 
-    with mlflow.start_run(run_name="ci-training"):
-        model = RandomForestClassifier(
+    mlflow.sklearn.autolog()
+
+    pipeline = Pipeline([
+        ("scaler", StandardScaler()),
+        ("model", RandomForestClassifier(
             n_estimators=100,
             random_state=42
-        )
-        model.fit(X_train, y_train)
+        ))
+    ])
 
-        y_pred = model.predict(X_test)
-        acc = accuracy_score(y_test, y_pred)
+    pipeline.fit(X_train, y_train)
 
-        mlflow.log_param("target_column", target_col)
-        mlflow.log_metric("accuracy", acc)
-        mlflow.sklearn.log_model(model, artifact_path="model")
+    y_pred = pipeline.predict(X_test)
+    acc = accuracy_score(y_test, y_pred)
 
-        print(f"Training completed. Accuracy: {acc:.4f}")
-
-
-if __name__ == "__main__":
-    main()
+    print(f"Accuracy (Pipeline): {acc:.4f}")
